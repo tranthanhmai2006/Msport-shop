2d761fdaa21332b9129a63116c6efb1456a7c6cb
Trần Thanh Mai <tranthanhmai@Trans-MacBook-Pro.local>
2023-07-16 09:29:49 +0700

    m

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    autoplay: {
+        delay: 5000,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
