71c9e185c8101a6ed56707608bec8c1807c41bf2
Trần Thanh Mai <tranthanhmai@Trans-MacBook-Pro.local>
2023-09-26 21:13:31 +0700

    update

--- js/contact.js
+++ js/contact.js
@@ -1,13 +1,12 @@
 function sendMail() {
-    let parameters = {
-        name: document.getElementById('name').value,
-        email: document.getElementById('email').value,
-        comment: document.getElementById('comment').value
+    var parameters = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        comment: document.getElementById("comment").value
     }
-    emailjs.send("service_py4xpbq", "template_f7kzcvy", parameters)
-        .then(function (response) {
-            console.log('SUCCESS!', response.status, response.text);
-        }, function (error) {
-            console.log('FAILED...', error);
-        });
+    emailjs.send('service_03funsi', 'template_krr5x9l', parameters)
+        .then(res => {
+            alert("Sent")
+        }
+        );
 }
